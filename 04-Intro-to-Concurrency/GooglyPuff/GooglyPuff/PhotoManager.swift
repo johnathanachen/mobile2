c82149c78ead8eb13a9a539b5bfaedd6423a795e
Johnathan Chen <johnathanchenn@outlook.com>
2017-10-30 15:56:45 -0700

    finished

--- 04-Intro-to-Concurrency/GooglyPuff/GooglyPuff/PhotoManager.swift
+++ 04-Intro-to-Concurrency/GooglyPuff/GooglyPuff/PhotoManager.swift
@@ -1,25 +1,3 @@
-/*
- * Copyright (c) 2016 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 import Foundation
 
 // Notification when new photo instances are added
@@ -42,15 +20,25 @@ class PhotoManager {
     return _sharedManager
   }
   
+
+  fileprivate let concurrentPhotoQueue =
+    DispatchQueue(label: "com.raywenderlich.GooglyPuff.photoQueue", attributes: .concurrent)
+    
   fileprivate var _photos: [Photo] = []
   var photos: [Photo] {
-    return _photos
+    var photosCopy: [Photo]!
+    concurrentPhotoQueue.sync {
+        photosCopy = self._photos
+    }
+    return photosCopy
   }
   
   func addPhoto(_ photo: Photo) {
-    _photos.append(photo)
-    DispatchQueue.main.async {
-      self.postContentAddedNotification()
+    concurrentPhotoQueue.async(flags: .barrier) {
+        self._photos.append(photo)
+        DispatchQueue.main.async {
+            self.postContentAddedNotification()
+        }
     }
   }
   
