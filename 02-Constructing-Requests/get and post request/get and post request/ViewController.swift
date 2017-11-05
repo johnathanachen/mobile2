6160eee7887010dfbce393c6992bde7585ed4bb1
Johnathan Chen <johnathanchenn@outlook.com>
2017-11-04 22:17:47 -0700

    put request

--- 02-Constructing-Requests/get and post request/get and post request/ViewController.swift
+++ 02-Constructing-Requests/get and post request/get and post request/ViewController.swift
@@ -30,7 +30,30 @@ class ViewController: UIViewController {
     }
     
     @IBAction func PUT(_ sender: UIButton) {
-    }
+        let parameters = ["twitter": "@johnathan", "facebook": "johnathanachen"]
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
+        request.httpBody = httpBody
+       
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let reponse = response {
+                print(response)
+            }
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+        }
+    }.resume()
+}
     
     override func viewDidLoad() {
         super.viewDidLoad()
