f0d09b48e20181080ab859c54b428663db71a4d5
Johnathan Chen <johnathanchenn@outlook.com>
2017-11-06 15:07:55 -0800

    ser

--- 02-Constructing-Requests/network_request/network_request/ViewController.swift
+++ 02-Constructing-Requests/network_request/network_request/ViewController.swift
@@ -10,15 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func GETButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func POSTButton(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
 
 
 }
